e1bace178e17dc4ee0be82b423f5e005aa02065d
chidambaram <ctchidu@gmail.com>
2020-01-07 09:52:59 +0530

    add camera list change

--- aframe/build/aframe-ar.js
+++ aframe/build/aframe-ar.js
@@ -8412,8 +8412,63 @@ AFRAME.registerComponent('arjs-anchor', {
 
                     // create select camera UI
                     var selectCameraButton = document.createElement('button');
-					var selectList = document.createElement("select");
-					selectList.id = "mySelect";
+					var videoElement = document.querySelector('#arjs-video');
+					var videoSelect = document.createElement("select");
+					videoSelect.id = "mySelect";
+					getStream().then(getDevices).then(gotDevices);
+					
+					function getDevices() {
+						// AFAICT in Safari this only gets default devices until gUM is called :/
+						return navigator.mediaDevices.enumerateDevices();
+						}
+						
+						function gotDevices(deviceInfos) {
+					  window.deviceInfos = deviceInfos; // make available to console
+					  console.log('Available input and output devices:', deviceInfos);
+					  for (const deviceInfo of deviceInfos) {
+						const option = document.createElement('option');
+						option.value = deviceInfo.deviceId;
+						 if (deviceInfo.kind === 'videoinput') {
+						  option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+						  videoSelect.appendChild(option);
+						}
+					  }
+					}
+
+					function getStream() {
+					  if (window.stream) {
+						window.stream.getTracks().forEach(track => {
+						  track.stop();
+						});
+					  }
+					  const videoSource = videoSelect.value;
+					  const constraints = {
+						video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+					  };
+					  return navigator.mediaDevices.getUserMedia(constraints).
+						then(gotStream).catch(handleError);
+					}
+
+					function gotStream(stream) {
+					  window.stream = stream; // make stream available to console
+					  videoSelect.selectedIndex = [...videoSelect.options].
+						findIndex(option => option.text === stream.getVideoTracks()[0].label);
+					  videoElement.srcObject = stream;
+					}
+
+function handleError(error) {
+  console.error('Error: ', error);
+}
+
+
+						
+						
+						
+						
+						
+						
+					
+					
                     selectCameraButton.innerText = 'Switch camera';
                     selectCameraButton.setAttribute('style', 'background-color: grey; color: white');
                     selectCameraButton.classList.add('arjs-select-camera');
@@ -8466,7 +8521,7 @@ AFRAME.registerComponent('arjs-anchor', {
                     });
 
                     containerElement.appendChild(selectCameraButton);
-					containerElement.appendChild(selectList);
+					containerElement.appendChild(videoSelect);
                 }, 1000);
             }
         }, 1000 / 60)
